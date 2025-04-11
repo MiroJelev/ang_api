24327526624fb7c6fdc93e41a4135e028825e539
Miro Zhelev <mirobond3@gmail.com>
2025-04-11 11:53:53 +0300

    Add First view

--- backend_api/api/views.py
+++ backend_api/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from backend_api.api.models import Country
+from backend_api.api.serializers import CountrySerializer
 
 # Create your views here.
+
+def country_list(request):
+    countries = Country.objects.all()
+    serializer = CountrySerializer(countries, many=True)
+    return JsonResponse({
+        'data': serializer.data
+    })
