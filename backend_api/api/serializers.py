8e4d8a5a75a8c34b3b230410bb381dfab4cd17b1
miro <miro@localhost.localdomain>
2025-04-19 22:56:14 +0300

    Update Serializers

--- backend_api/api/serializers.py
+++ backend_api/api/serializers.py
@@ -1,7 +1,24 @@
-from backend_api.api.models import Country
+from backend_api.api.models import Encompasses, Country, City, Politics
 from rest_framework import serializers
 
+class ContinentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Encompasses
+        fields = "__all__"
+
+class PoliticsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Politics
+        fields = "__all__"
+
 class CountrySerializer(serializers.ModelSerializer):
+    politics = PoliticsSerializer()
+    continents = ContinentsSerializer()
     class Meta:
         model = Country
-        fields = ["name","code","capital","province","area","population"]
+        fields = "__all__"
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = "__all__"
