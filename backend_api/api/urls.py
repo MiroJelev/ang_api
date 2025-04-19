d4368402b15d9cd4e1f16ad3d4a024ea665e4b4d
miro <miro@localhost.localdomain>
2025-04-19 22:56:29 +0300

    Add new Endpoints

--- backend_api/api/urls.py
+++ backend_api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from backend_api.api.views import CountryList
+from backend_api.api.views import CountryCityList, CountryList, PoliticsList
 
 urlpatterns = [
-    path('country/', CountryList.as_view())
+    path('country/', CountryList.as_view()),
+    path('policy/', PoliticsList.as_view()),
+    path('country_cities/', CountryCityList.as_view())
 ]
--- backend_api/api/views.py
+++ backend_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from backend_api.api.models import Country
-from backend_api.api.serializers import CountrySerializer
+from backend_api.api.models import Country, City, Politics
+from backend_api.api.serializers import CitySerializer, CountrySerializer, PoliticsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,6 +10,45 @@ from rest_framework import status
 class CountryList(APIView):
     serializer_class = CountrySerializer
     def get(self, request, format=None):
-        countries = Country.objects.all()
+        sql_query = """SELECT * FROM Mondial.country
+INNER JOIN Mondial.politics 
+ON Mondial.country.Code=Mondial.politics.Country
+INNER JOIN Mondial.encompasses
+ON Mondial.country.Code=Mondial.encompasses.Country
+WHERE (Government LIKE '%%monarch%%' 
+OR Government LIKE '%%principality%%')
+AND Continent Like 'Europe';"""
+
+        countries = Country.objects.prefetch_related('politics').raw(sql_query)
         serializer = CountrySerializer(countries, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
+        
+class PoliticsList(APIView):
+    serializer_class = PoliticsSerializer
+    def get(self, request, format=None):
+        sql_query = """SELECT * FROM Mondial.politics
+INNER JOIN Mondial.country 
+ON Mondial.country.Code=Mondial.politics.Country
+INNER JOIN Mondial.encompasses
+ON Mondial.country.Code=Mondial.encompasses.Country
+WHERE (Government LIKE '%%monarch%%' 
+OR Government LIKE '%%principality%%')
+AND Continent Like 'Europe';"""
+
+        policies = Politics.objects.raw(sql_query)
+        serializer = PoliticsSerializer(policies, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class CountryCityList(APIView):
+    serializer_class = CountrySerializer
+    def get(self, request, format=None):
+        if 'country_city' in request.query_params:
+            country_city = request.query_params['country_city']
+            if country_city is not None:
+                countries = Country.objects.filter(name=country_city)
+                serializer = CountrySerializer(countries, many=True)
+                return Response(serializer.data, status.HTTP_200_OK)
+            else:
+                return Response([], status.HTTP_204_NO_CONTENT)
+        else:
+            return Response([], status.HTTP_204_NO_CONTENT)
